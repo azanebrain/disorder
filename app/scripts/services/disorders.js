8f81d7ee81c0ce98c6e2f0cf3c4ca44154109513
AJ Zane <androiddreams@ajzane.com>
2014-10-03 17:45:35 -0700

    add factory for all unique tags

--- app/scripts/services/disorders.js
+++ app/scripts/services/disorders.js
@@ -7,6 +7,7 @@
  * # Disorders
  * Service in the disOrderApp.
  */
+ // Get all of the post data
 angular.module('disOrderApp')
   .factory('DisorderData', ['$resource',
     function($resource){
@@ -14,12 +15,20 @@ angular.module('disOrderApp')
         query: {method:'GET', params:{}, isArray:true}
       });
     }]);
+ // Get all of the tags
+angular.module('disOrderApp')
+  .factory('DisorderDataTaxonomies', ['$resource',
+    function($resource){
+      return $resource('http://disorder.dev/wp/wp-json/taxonomies/post_tag/terms', {}, {
+        query: {method:'GET', params:{}, isArray:true}
+      });
+    }]);
 // Make this more dynamic: get posts, get tags, get X
-//get tags
+// This will get all of the terms as an object:
 // http://disorder.dev/wp/wp-json/taxonomies/post_tag/terms
 
 angular.module('disOrderApp')
-  .service('disordersFact', ['DisorderData', function disordersFact(DisorderData) {
+  .service('disordersFact', ['DisorderData', 'DisorderDataTaxonomies', function disordersFact(DisorderData , DisorderDataTaxonomies) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     var Disorders = {};
 
@@ -30,6 +39,7 @@ angular.module('disOrderApp')
     //Create the array of all tags here
     // Disorders.uniqueTags = somefunction(Disorders.list);
     console.log("Create the array of all tags here, in the DisorderData Factory");
+    Disorders.uniqueTags = DisorderDataTaxonomies.query();
 
     // Return the Disorders object
     return Disorders;
